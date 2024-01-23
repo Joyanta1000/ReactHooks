901b2c708a0d56770ce10264961afa2a1e97fcdc
Joyanta1000 <joyanta955@gmail.com>
2024-01-23 12:38:13 +0600

    useContext used

---
+++ src/components/SomeComponent.jsx
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react';
+import ThemeContext from './ThemeContext';
+
+const SomeComponent = () => {
+  const { theme, setTheme } = useContext(ThemeContext);
+
+  return (
+    <div>
+      <p>Current Theme: {theme}</p>
+      <button onClick={() => setTheme(theme === 'light' ? 'dark' : 'light')}>
+        Toggle Theme
+      </button>
+    </div>
+  );
+};
+
+export default SomeComponent;
